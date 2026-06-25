bb29f87d5d1aacd530e08a8c9095dd4e9d8e4464
Agustin <agustinfuentesmartinez05@gmail.com>
2026-06-25 20:49:50 -0300

    deployando frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://recetario-familiar-ow37.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
