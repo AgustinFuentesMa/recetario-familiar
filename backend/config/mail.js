0d8068401ea56e23cd36f553e7848d9e5ccc75f9
Agustin <agustinfuentesmartinez05@gmail.com>
2026-06-26 13:45:38 -0300

    Corregir otra vez los mail

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,22 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("SMTP listo");
-  }
 });
 
 module.exports = transporter;
