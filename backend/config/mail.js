af26238be8d07c759466ea5b9c0aec9a67754297
Agustin <agustinfuentesmartinez05@gmail.com>
2026-06-26 14:46:19 -0300

    Analizando mail

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,12 +1,9 @@
 const nodemailer = require("nodemailer");
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "OK" : "NO");
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
