18ccd2106f97b368d3da6efbf510ad826a50ee5c
Agustin <agustinfuentesmartinez05@gmail.com>
2026-06-26 13:12:04 -0300

    Corregir mail.js

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
+ console.log(process.env.EMAIL_USER)
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
