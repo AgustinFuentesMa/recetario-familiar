0b4d0dec04f437af15031699f5cf38ebf5b45245
Agustin <agustinfuentesmartinez05@gmail.com>
2026-06-25 21:07:30 -0300

    Corregir deploy del frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://recetario-familiar-ow37.onrender.com/",
+  baseURL: "https://recetario-familiar-ow37.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
